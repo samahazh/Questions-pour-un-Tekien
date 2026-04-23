760215af1a7b663938a2bcf542e02c5afae7f659
samahazh <samah.azhani1@etu.cyu.fr>
2026-04-24 00:37:26 +0200

    Update qcm.h

--- qcm.h
+++ qcm.h
@@ -1,1 +1,25 @@
+#ifndef QCM_H
+#define QCM_H
+
+typedef struct {
+    int points_negatifs;
+    int reponses_multiples;
+    int mode_sequentiel;
+} Parametres; 
+
+typedef struct {
+    char enonce[256];
+    char propositions[4][256];
+    int reponses_correctes[4];
+    int nb_propositions;
+} Question;
+
+typedef struct {
+    char nom[128];
+    Parametres regles;
+    Question questions[50];
+    int nb_questions;
+}QCM;
+
+#endif
 
