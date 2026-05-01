dc5480ae38c22edae990af7d9b40163160400259
Atcchaya <atcchayarasamuraly@outlook.com>
2026-05-01 16:13:04 +0200

    Update student.c

--- student.c
+++ student.c
@@ -1,3 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "qcm.h"
+#include "student.h"
+
 void launchStudentMode() {
     QCM monQuiz;
     char nomFichier[100];
@@ -7,6 +12,7 @@ void launchStudentMode() {
     printf("Nom du fichier QCM à ouvrir (ex: test.bin) : ");
     scanf("%s", nomFichier);
 
+    // 1. Lecture du fichier
     FILE *f = fopen(nomFichier, "rb");
     if (f == NULL) {
         printf("Erreur : Impossible de trouver le fichier %s\n", nomFichier);
@@ -15,7 +21,7 @@ void launchStudentMode() {
     fread(&monQuiz, sizeof(QCM), 1, f);
     fclose(f);
 
-    // --- DEROULEMENT DU TEST ---
+    // 2. Déroulement des questions
     for (int i = 0; i < monQuiz.nbTotalQuestions; i++) {
         int choixEleve;
         printf("\nQuestion %d : %s\n", i + 1, monQuiz.listeQuestions[i].enonce);
@@ -27,6 +33,7 @@ void launchStudentMode() {
         printf("Votre réponse (1-4) : ");
         scanf("%d", &choixEleve);
 
+        // Vérification
         if (monQuiz.listeQuestions[i].reponsesVraies[choixEleve - 1] == 1) {
             printf("Bravo ! C'est juste.\n");
             scoreFinal += 1;
@@ -39,7 +46,7 @@ void launchStudentMode() {
         }
     }
 
-    // --- AFFICHAGE DE LA CORRECTION ---
+    // 3. Affichage de la correction détaillée
     printf("\n--- RECAPITULATIF DES BONNES REPONSES ---\n");
     for (int i = 0; i < monQuiz.nbTotalQuestions; i++) {
         printf("Q%d : %s\n", i + 1, monQuiz.listeQuestions[i].enonce);
@@ -52,12 +59,11 @@ void launchStudentMode() {
         }
     }
 
-    // --- RESULTAT FINAL ---
+    // 4. Calcul de la note finale
     float noteSur20 = (scoreFinal / monQuiz.nbTotalQuestions) * 20;
     if (noteSur20 < 0) noteSur20 = 0;
 
     printf("\n--- RESULTAT ---\n");
     printf("Votre note finale : %.2f / 20\n", noteSur20);
     printf("--------------------------------\n\n");
-}
 }
